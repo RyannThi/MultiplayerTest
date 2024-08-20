bedb50e1f44a41002c8156a1d13a040e89aecae0
Ryann1908 <59339990+RyannThi@users.noreply.github.com>
2024-08-20 08:34:55 -0300

    amo

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
 {
 
     InputAction movement, turning;
+    Animator animator;
     private void Awake()
     {
         InputActionsGame inputsGame = new InputActionsGame();
@@ -21,7 +22,8 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        transform.position += new Vector3(Random.Range(-15, 15), 0, Random.Range(0, 15));
     }
 
     // Update is called once per frame
@@ -31,5 +33,14 @@ public class PlayerController : MonoBehaviour
             movement.ReadValue<float>()*5*Time.deltaTime);
         transform.Rotate(0,
             turning.ReadValue<float>()*180*Time.deltaTime,0);
+
+        if (movement.ReadValue<float>() != 0) 
+        {
+            animator.SetBool("walking", true);
+        }
+        else
+        {
+            animator.SetBool("walking", false);
+        }
     }
 }
